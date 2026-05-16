c30aae239fb9a70f359e36c099b8388722b8f7bf
SoyaOhnishi <soya.ohnishi@gmail.com>
2026-05-16 13:32:12 +0900

    fix

--- hw/rl78/tau.c
+++ hw/rl78/tau.c
@@ -241,14 +241,14 @@ static void rl78_tau_start_timer(RL78TAUState *s, const uint8_t channel,
         const uint64_t timer_ns = (uint64_t)(clock_ns * (count + 1));
         const uint64_t expire_time =
             qemu_clock_get_ns(QEMU_CLOCK_VIRTUAL) + timer_ns;
+        static uint64_t before_expire_time = 0;
 
         s->channel[channel].last_timer_expire = expire_time;
-        const uint64_t before_expire_time = timer_expire_time_ns(&s->channel[channel].timer);
         const uint64_t after_expire_time = expire_time;
         const uint64_t expire_time_diff = after_expire_time - before_expire_time;
         const uint64_t squezed_timerup_count = expire_time_diff / clock_ns;
 
-        if(squezed_timerup_count > 2) {
+        if(before_expire_time > 0 && squezed_timerup_count > 2) {
             qemu_log("before: %lu, after: %lu, diff: %lu, squezed_timerup_count: %lu\n", before_expire_time, after_expire_time, expire_time_diff, squezed_timerup_count);
         }
 
@@ -257,6 +257,8 @@ static void rl78_tau_start_timer(RL78TAUState *s, const uint8_t channel,
         if (s->tmr[channel].timer_behavior == 1) {
             qemu_set_irq(s->irqs[channel], 1);
         }
+
+        before_expire_time = expire_time;
     }
 }
 

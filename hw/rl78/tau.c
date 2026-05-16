c71c9cc30bc491bb9c6a2e0fe0b72e2956686dca
SoyaOhnishi <soya.ohnishi@gmail.com>
2026-05-16 13:39:43 +0900

    fix

--- hw/rl78/tau.c
+++ hw/rl78/tau.c
@@ -950,6 +950,9 @@ static void rl78_tau_update_timer(QEMUTimer *timer, uint64_t *last_expire_time,
     const uint64_t timer_ns = (uint64_t)(timer_duration * 1000 * 1000 * 1000);
     const uint64_t expire_time = *last_expire_time + timer_ns;
 
+    const uint64_t expire_time_diff = expire_time - *last_expire_time;
+    const uint64_t squezed_timerup_count = expire_time_diff / timer_ns;
+    qemu_log("before: %lu, after: %lu, diff: %lu, squezed_timerup_count: %lu\n", *last_expire_time, expire_time, expire_time_diff, squezed_timerup_count);
 
     *last_expire_time = expire_time;
     timer_mod(timer, expire_time);
